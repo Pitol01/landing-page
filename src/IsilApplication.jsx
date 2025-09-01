64dacd51bc3c066f79c3b561b563e925ef4cdbfa
DanielK2027I <juandanielyllanescaldas@gmail.com>
2025-09-01 20:20:44 +0000

    integre ubicacion del header xd

--- src/IsilApplication.jsx
+++ src/IsilApplication.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { HomePage } from "./features/home/HomePage";
 import { ActividadesPage } from "./features/actividades/ActividadesPage";
-import { AppLayout } from "./layouts/AppLayout";
+import { AppLayout } from "./ui/layouts/AppLayout";
+import { Eventos } from "./features/Eventos/Eventos";
 
 export const IsilApplication = () => {
   return (
@@ -11,6 +12,7 @@ export const IsilApplication = () => {
           <Route element={<AppLayout />}>
             <Route index path="/" element={<HomePage />} />
             <Route path="actividades" element={<ActividadesPage />} />
+            <Route path="eventos" element={<Eventos />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/ui/components/common/Header.tsx
+++ src/ui/components/common/Header.tsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+import "./header.css"
+
 export const Header = () => {
-  return <header>Header</header>;
+  const [open, setOpen] = useState(false);
+  return <header><nav className="navbar">
+      <div className="navbar-container">
+      
+        <h1 className="navbar-logo">MiApp</h1>
+
+    
+        <ul className={`navbar-links ${open ? "active" : ""}`}>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#services">Servicios</a></li>
+          <li><a href="#about">Nosotros</a></li>
+          <li><a href="#contact">Contacto</a></li>
+        </ul>
+
+        <button className="navbar-toggle" onClick={() => setOpen(!open)}>
+          {open ? "✖" : "☰"}
+        </button>
+      </div>
+    </nav></header>;
 };
