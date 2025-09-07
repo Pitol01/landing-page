528ee7c1fbf08f19d3bf21ccea5069eef37de0ad
kokonut <castaneda.jair.13@gmail.com>
2025-09-07 16:48:49 -0500

    feat: rutas agregadas, y navbar funcional

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./ui/css/index.css";
 import { IsilApplication } from "./IsilApplication";
 
 createRoot(document.getElementById("root")).render(
--- src/ui/components/common/Header.tsx
+++ src/ui/components/common/Header.tsx
@@ -1,24 +1,58 @@
 import { useState } from "react";
-import "./header.css"
+import "./header.css";
+import { Link } from "react-router";
+import { IsilIconSvg } from "../../../assets/isil-icon/IsilIconSvg";
 
-export const Header = () =>  {
+const navLinks = [
+  {
+    label: "¿Qué hacemos?",
+    to: "/about",
+  },
+  {
+    label: "Actividades",
+    to: "/activities",
+  },
+  {
+    label: "Calculadora de promedios",
+    to: "/calculate-promedio",
+  },
+  {
+    label: "Eventos",
+    to: "/events",
+  },
+  {
+    label: "Recursos para programadores",
+    to: "/resources",
+  },
+  {
+    label: "Créditos",
+    to: "/credits",
+  },
+];
+
+export const Header = () => {
   const [open, setOpen] = useState(false);
-  return <header><nav className="navbar">
-      <div className="navbar-container">
-      
-        <h1 className="navbar-logo">MiApPPP</h1>
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="navbar-logo">
+            <IsilIconSvg />
+          </div>
 
-    
-        <ul className={`navbar-links ${open ? "active" : ""}`}>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#services">Servicios</a></li>
-          <li><a href="#about">Nosotros</a></li>
-          <li><a href="#contact">Contacto</a></li>
-        </ul>
+          <ul className={`navbar-links ${open ? "active" : ""}`}>
+            {navLinks.map(({ label, to }, i) => (
+              <Link key={i} to={to}>
+                {label}
+              </Link>
+            ))}
+          </ul>
 
-        <button className="navbar-toggle" onClick={() => setOpen(!open)}>
-          {open ? "✖" : "☰"}
-        </button>
-      </div>
-    </nav></header>;
+          <button className="navbar-toggle" onClick={() => setOpen(!open)}>
+            {open ? "✖" : "☰"}
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
 };
