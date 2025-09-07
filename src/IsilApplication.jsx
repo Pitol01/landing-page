2182ea9f86ed57ee34f7594f3d0ba87a2db3d927
kokonut <castaneda.jair.13@gmail.com>
2025-09-07 16:49:21 -0500

    fix path

--- src/IsilApplication.jsx
+++ src/IsilApplication.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { HomePage } from "./features/home/HomePage";
 import { ActividadesPage } from "./features/actividades/ActividadesPage";
 import { AppLayout } from "./ui/layouts/AppLayout";
-import { Eventos } from "./features/eventos/Eventos";
+import { Eventos } from "./features/Eventos/Eventos";
 
 export const IsilApplication = () => {
   return (
